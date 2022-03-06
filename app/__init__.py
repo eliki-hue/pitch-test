31a337666379610bd2b6bc6554130ccf0f8b65bf
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-06 05:37:43 +0300

    created initializer app

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from flask import Flask
 from config import DevConfig
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 
 
 
-app =Flask(__name__, instance_relative_config=True)
+
 
 Bootstrap(app)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+db = SQLAlchemy()
 
 
-from app import views
+
+
 
 
 bootstrap = Bootstrap()
+
+def initializer(config_name):
+    app =Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config_options)
+
+    from app import views
