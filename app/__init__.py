be5454beab839dd6a9e569159a20c72193cca9bf
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-06 05:30:58 +0300

    initialization: initialized the app using the Flask class and bootstraping the app

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import DevConfig
+from flask_bootstrap import Bootstrap
+
+
+
+app =Flask(__name__, instance_relative_config=True)
+
+Bootstrap(app)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+
+from app import views
+
+
+bootstrap = Bootstrap()
