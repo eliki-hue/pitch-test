dd1341b1cbfb54afa370f0af24f57833024d7542
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-07 05:35:34 +0300

    user: added property decorator

--- app/models.py
+++ app/models.py
@@ -16,3 +16,10 @@ class User:
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    def __repr__(self):
+        return f'User {self.username}'
