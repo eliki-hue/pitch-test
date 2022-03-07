56fa55dfceefffaef29e7c665fb1e73f03ecaf0a
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-07 22:40:53 +0300

    added blocks to base html

--- app/models.py
+++ app/models.py
@@ -14,8 +14,7 @@ class User:
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
-    bio = db.Column(db.String(255))
+   
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
 
