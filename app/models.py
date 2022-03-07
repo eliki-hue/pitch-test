c101e1d0ef92496beb77c42f154c8d5e4531aa55
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-07 05:38:41 +0300

    user: added password verifier

--- app/models.py
+++ app/models.py
@@ -25,6 +25,9 @@ class User:
     def password(self, password):
         self.pass_secure = generate_password_hash(password)
 
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
 
     def __repr__(self):
         return f'User {self.username}'
