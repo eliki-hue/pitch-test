5a3859ba2d4c38264cb3e809c147e613b72d585f
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-08 18:27:31 +0300

    set-up mail_message function to send the email

--- app/email.py
+++ app/email.py
@@ -1,3 +1,12 @@
+import imp
 from flask_mail import Message
 from flask import render_template
 from . import mail
+
+def mail_message(Subject, template,to, **kwargs):
+    sender_email = 'eliki13720@gmail.com'
+    email = Message(subject='', sender=sender_email, recipients=[])
+    email.body =render_template(template + ".txt", **kwargs)
+    email.html = render_template(template + ".html", **kwargs)
+    mail.send(email)
+    
