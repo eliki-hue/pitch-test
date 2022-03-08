c9a4d8b2ba4d261c6eba9d7330931b9478da8bbc
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-08 14:42:28 +0300

    registration:passed user data into the database

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
+from crypt import methods
 from .models import User
 from .form import RegistrationForm
 from flask import render_template, request, url_for
-from . import app
+from . import app, db
 
 
 
@@ -16,9 +17,23 @@ def index():
 @app.route("/register")
 def register():
     Registration= RegistrationForm()
+    
 
     return render_template('form.html', Registration=RegistrationForm)
 
-@app.route('/success', methods=['post'])
+@app.route('/success', methods=['POST'])
 def success():
+    if request.method == "POST":
+
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('psw')
+        print(username)
+        print(email)
+        print(password)
+        data = User(username,email,password)
+
+        db.session.add(data)
+        db.session.commit()
+
     return render_template('success.html')
