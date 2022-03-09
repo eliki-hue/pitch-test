907b150e90fdb08867d6fd526111da2349686422
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-09 12:34:21 +0300

    commented out lon session

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,7 @@ db.init_app(app)
 mail.init_app(app)
 
 login_manager = LoginManager()
-login_manager._session_protection ='strong'
+# login_manager._session_protection ='strong'
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,99 +1,71 @@
-from flask_login import current_user, login_required, login_user,logout_user
 from .email import mail_message
 from .models import User
 from .form import RegistrationForm
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, flash
 from . import app, db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
-# @app.route('/')
-# def index():
-
-#     return render_template('index.html')
-
 @app.route('/')
-def home():
+def index():
+
     return render_template('index.html')
 
 
-# @app.route("/register")
-# def register():
-#     Registration= RegistrationForm()
+@app.route("/register")
+def register():
+    Registration= RegistrationForm()
     
 
-#     return render_template('form.html', Registration=Registration)
+    return render_template('form.html', Registration=RegistrationForm)
 
-# @app.route('/success', methods=['GET','POST'])
-# def success():
-#     if request.method == "POST":
+@app.route('/success', methods=['GET','POST'])
+def success():
+    if request.method == "POST":
 
-#         username = request.form.get('username')
-#         email = request.form.get('email')
-#         password = request.form.get('psw')
-#         if db.session.query(User).filter(User.email ==email).count()==0:
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('psw')
+        if db.session.query(User).filter(User.email ==email).count()==0:
 
-#             print(username)
-#             print(email)
-#             print(password)
-#             data = User(username,email,password=generate_password_hash(password, method='sha256'))
+            print(username)
+            print(email)
+            print(password)
+            data = User(username,email,password=generate_password_hash(password, method='sha256'))
 
-#             db.session.add(data)
-#             db.session.commit()
-#             try:
-#                 mail_message(username)
-#             except:
-#                 pass
-#             return render_template('success.html')
+            db.session.add(data)
+            db.session.commit()
+            try:
+                mail_message(username)
+            except:
+                pass
+            return render_template('success.html')
     
-#         return render_template('form.html', text ='User with that email address already exist')
+        return render_template('form.html', text ='User with that email address already exist')
 
 
-# @app.route('/login' )
-# def login():
+@app.route('/login' )
+def login():
+    return render_template('login.html')
 
-#     return render_template('login.html')
 
-
-# @app.route('/profile', methods=['GET','POST'])
-# def profile():
-#     if request.method == "POST":
-        
+@app.route('/profile', methods=['GET','POST'])
+def profile():
+    if request.method == "POST":
 
        
-#         email = request.form.get('email')
+        email = request.form.get('email')
+        password = request.form.get('psw')
 
-#         password = request.form.get('psw')
-
-#         user = User.query.filter_by(email=email).first()
-#         if not user or not check_password_hash(user.password, password):
-#             text='Please check your login details and try again.'
-#             return render_template('login.html', text=text)
-#         login_user(user)
-#     return render_template('profile.html',user=user)
-            
-
-# @app.route('/main')
-# @login_required
-# def main():
-#     return render_template('main.html',name=current_user)
-
-
-# # @app.route('/logout')
-# # @login_required
-# # def logout():
-# #     logout_user()
-# #     return render_template('index.html')
+        user = User.query.filter_by(email=email).first()
+        if not user or not check_password_hash(user.password, password):
+            text='Please check your login details and try again.'
+            return render_template('login.html', text=text)
+        return render_template('profile.html',user=user)
 
             
-    
+@app.route('/pitchForm')
+def pitchForm():
 
-
-            
-
-            
-# @app.route('/pitchForm')
-# def pitchForm():
-
-#     return render_template('pitchForm.html')
+    return render_template('pitchForm.html')
