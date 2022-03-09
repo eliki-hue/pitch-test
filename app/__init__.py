f5c53e9397aa0f31d86fdc126030f91e914253b3
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-09 12:12:59 +0300

    removed login manager

--- app/__init__.py
+++ app/__init__.py
@@ -37,8 +37,8 @@ db.init_app(app)
 mail.init_app(app)
 
 login_manager = LoginManager()
-login_manager._session_protection ='strong'
-login_manager.login_view = 'auth.login'
+# login_manager._session_protection ='strong'
+# login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 from app import views
 
