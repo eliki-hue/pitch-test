7b76e6945abfe8436ecdbc1456ecef1273bd426a
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-10 10:51:07 +0300

    formatted the home page and applyied blue background colour

---
+++ app/pitchPull.py
@@ -0,0 +1,6 @@
+# from ..app import db
+# from .models import Pitch
+
+# def pitcher():
+#     data = Pitch.query
+#     print(data)
--- app/templates/pitchPull.py
+++
@@ -1,7 +0,0 @@
-from app import db
-from ..models import Pitch
-
-pitchesArray =[]
-
-
-# data = Pitch.query.filter_by(email=email).first()
